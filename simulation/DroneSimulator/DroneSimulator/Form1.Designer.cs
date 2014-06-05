d9f0be5f724ba65c2f5a18487e06f19cc919c131
gagichce <gagichce@mcmaster.ca>
2014-06-05 10:02:31 -0400

    initial commit of simulation code written by Ash

---
+++ simulation/DroneSimulator/DroneSimulator/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DroneSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoadWayPoints = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.cpu_timer = new System.Windows.Forms.Timer(this.components);
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblDEBUG = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadWayPoints
+            // 
+            this.btnLoadWayPoints.Location = new System.Drawing.Point(38, 12);
+            this.btnLoadWayPoints.Name = "btnLoadWayPoints";
+            this.btnLoadWayPoints.Size = new System.Drawing.Size(119, 39);
+            this.btnLoadWayPoints.TabIndex = 0;
+            this.btnLoadWayPoints.Text = "Load Control";
+            this.btnLoadWayPoints.UseVisualStyleBackColor = true;
+            this.btnLoadWayPoints.Click += new System.EventHandler(this.btnLoadWayPoints_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(188, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(119, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Reset Drone CPU";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(333, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(119, 39);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Location = new System.Drawing.Point(1015, 75);
+            this.tbLog.MaxLength = 65543;
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLog.Size = new System.Drawing.Size(260, 617);
+            this.tbLog.TabIndex = 4;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(1012, 57);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(25, 13);
+            this.lblPath.TabIndex = 6;
+            this.lblPath.Text = "Log";
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(18, 57);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(963, 680);
+            this.PictureBox1.TabIndex = 7;
+            this.PictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 753);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Time:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(54, 753);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(27, 13);
+            this.lblTime.TabIndex = 9;
+            this.lblTime.Text = "0.0s";
+            // 
+            // cpu_timer
+            // 
+            this.cpu_timer.Interval = 20;
+            this.cpu_timer.Tick += new System.EventHandler(this.cpu_timer_Tick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1156, 698);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 39);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save Log File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblDEBUG
+            // 
+            this.lblDEBUG.AutoSize = true;
+            this.lblDEBUG.Location = new System.Drawing.Point(199, 753);
+            this.lblDEBUG.Name = "lblDEBUG";
+            this.lblDEBUG.Size = new System.Drawing.Size(45, 13);
+            this.lblDEBUG.TabIndex = 11;
+            this.lblDEBUG.Text = "DEBUG";
+            this.lblDEBUG.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 775);
+            this.Controls.Add(this.lblDEBUG);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnLoadWayPoints);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Drone Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadWayPoints;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer cpu_timer;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblDEBUG;
+    }
+}
+
---
+++ simulation/DroneSimulator/DroneSimulator/Form1.cs
@@ -0,0 +1,577 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace DroneSimulator
+{
+    public partial class Form1 : Form
+    {
+        struct coordinate
+        {
+            public double x;
+            public double y;
+        };
+
+        struct INSTRUCTION
+        {
+            public double x;
+            public double y;
+            public int timer;
+            public int led;
+        };
+
+        //center point
+        int center_x = 200;
+        int center_y = 200;
+        //boundary
+        const double ulx = 0;
+        const double uly = 0;
+        const double brx = 960;
+        const double bry = 680;
+
+        const Boolean debug = false;
+        
+        //Random variant
+        Boolean environment_random;
+        double random_x = 0;
+        double random_y = 0;
+        Random r;
+        double random_rate = 2;
+
+        //delayed x n y rate
+        double r_x = 0.05;
+        double r_y = 0.2;
+
+        //cpu counter
+        Boolean start;
+        //led status
+        Boolean led;
+
+        //general variables
+        private int i;
+        private System.Drawing.Graphics g;
+        private System.Drawing.Brush plane;
+        private System.Drawing.Brush plane_led;
+        private System.Drawing.Brush trail;
+        private coordinate[] map;
+        private double ax, ay;
+        private double px, py;
+        private double timer;
+
+
+        //cpu related
+        UInt32 cpu_counter;
+        int setTime;
+        int cpu_pt = 0;
+        INSTRUCTION [] instruction;
+        int clockspeed = 100; // 100 ms per tick = 10Hz 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //initialize graphical system
+            g = PictureBox1.CreateGraphics();
+            plane = Brushes.Red;
+            trail = Brushes.Yellow;
+            plane_led = Brushes.Green;
+
+
+            //initialize physical system
+            map = new coordinate[20];
+            start = false;
+            timer = 0.0;
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+        
+            //initialize simulated cpu
+            cpu_pt = -1;
+            cpu_counter = 0;
+            cpu_timer.Enabled = false;
+            instruction = new INSTRUCTION[1000];
+            clear_instructions();
+
+
+            //enable environment noise
+            environment_random = true;
+            r = new Random();
+
+            //load settings, SOFTWARE TEAM PLEASE DO NOT TOUCH THE CONFIG FILE
+            //Result can be far off if settings are changed.
+            read_config();
+            cpu_timer.Interval = clockspeed;
+            position_reset();
+
+            //debug flag
+            if (debug)
+                lblDEBUG.Visible = true;
+
+
+
+            tbLog.Text += "Initalized" + Environment.NewLine;
+        }
+
+        void read_config()
+        {
+            string fileContent = "";
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("config.inf");
+                fileContent = sr.ReadToEnd();
+
+                string [] lines = fileContent.Split('\n');
+                string [] temp;
+
+                for (i = 0; i < lines.Length; i++)
+                {
+                    temp = lines[i].Split('=');
+                    switch (temp[0])
+                    {
+                        case "Starting_Position_X": 
+                            {
+                                center_x = Convert.ToInt32(temp[1]);
+                                break;
+                            }
+                        case "Starting_Position_Y":
+                            {
+                                center_y = Convert.ToInt32(temp[1]);
+                                break;
+                            }
+                        case "Random_Environment":
+                            {
+                                if (temp[1].ToUpper() == "FALSE")
+                                    environment_random = false;
+                                else
+                                    environment_random = true;
+                                break;
+                            }
+                        case "Random_Rate":
+                            {
+                                random_rate = Convert.ToDouble(temp[1]);
+                                break;
+                            }
+                        case "X_Reactive_Rate":
+                            {
+                                r_x = Convert.ToDouble(temp[1]);
+                                break;
+                            }
+                        case "Y_Reactive_Rate":
+                            {
+                                r_y = Convert.ToDouble(temp[1]);
+                                break;
+                            }
+                        case "CPU_Clock_Speed":
+                            {
+                                clockspeed = Convert.ToInt32(temp[1]);
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+
+                }
+                sr.Close();
+            }
+            catch
+            {
+                System.IO.StreamWriter wr = new System.IO.StreamWriter("config.inf");
+                fileContent = "Starting_Position_X" + "=" + center_x.ToString() + Environment.NewLine;
+                fileContent += "Starting_Position_Y" + "=" + center_y.ToString() + Environment.NewLine;
+                fileContent += "Random_Environment" + "=" + environment_random.ToString() + Environment.NewLine;
+                fileContent += "Random_Rate" + "=" + random_rate.ToString() + Environment.NewLine;
+                fileContent += "X_Reactive_Rate" + "=" + r_x.ToString() + Environment.NewLine;
+                fileContent += "Y_Reactive_Rate" + "=" + r_y.ToString() + Environment.NewLine;
+                fileContent += "CPU_Clock_Speed" + "=" + clockspeed.ToString();
+                wr.Write(fileContent);
+                wr.Close();
+            }
+        }
+
+
+        //---------------------------------------------------------------------------------
+        //UI Buttons
+        //---------------------------------------------------------------------------------
+
+        //load cpu instructions
+        private void btnLoadWayPoints_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            string fileContent = "";
+
+           
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                    fileContent = sr.ReadToEnd();
+                   
+
+                    sr.Close();
+                }
+                catch 
+                {
+                    MessageBox.Show("Failed to read the file!");
+                    return;
+                }
+
+
+
+
+                try
+                {
+
+                    string[] temp = fileContent.Split('\n');
+                    for (i = 0; i < temp.Length; i++)
+                    {
+                        INSTRUCTION t = command_set(temp[i]);
+                        if (t.timer >= 0)
+                        {
+                            instruction[i] = t;
+                            instruction[i].x /= 10;
+                            instruction[i].y /= 10;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to compile the file!");
+                            return;
+                        }
+                    }
+
+
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to compile the file!");
+                    return;
+                }
+            }
+
+        }
+
+        //Flow control button
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            start = true;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            initalize_cpu();
+            tbLog.Text += "Start" + Environment.NewLine;
+
+        }
+
+        //Reset button
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ax = 0;
+            px = 0;
+            ay = 0;
+            py = 0;
+            position_reset();
+            timer2.Enabled = true;
+            cpu_counter = 0;
+            led = false;
+            cpu_timer.Enabled = false;
+            start = false;
+            timer = 0.0;
+
+            tbLog.Text += "Reset" + Environment.NewLine;
+        }
+        
+        //Save log file
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.IO.StreamWriter wr = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                    wr.Write(tbLog.Text);
+                    wr.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to write the file!");
+                    return;
+                }
+            }
+        }
+
+
+        //----------------------------------------------------
+        //CPU
+        //----------------------------------------------------
+
+        //CPU instruction reading
+        private void cpu_instruction()
+        {
+            cpu_counter++;
+            if (cpu_counter >= setTime)
+            {
+                cpu_counter = 0;
+                next_instruction();
+            }
+        }
+
+        private void next_instruction()
+        {
+            //critical section, disable interrupt
+            cpu_timer.Enabled = false;
+            cpu_pt++;
+
+            tbLog.Text += lblTime.Text + " Fetch next instruction, pointer at " + cpu_pt.ToString() + Environment.NewLine; 
+            if (instruction[cpu_pt].x == 0 && instruction[cpu_pt].y == 0 && instruction[cpu_pt].timer == 0)
+            {
+                tbLog.Text += lblTime.Text + " Finish executing all the commands, now stop" + Environment.NewLine;
+
+               
+                led = false;
+             
+
+                cpu_pt = 0;
+                setTime = 0;
+
+                ax = 0;
+                ay = 0;
+
+
+                return; 
+             }
+
+            ax = instruction[cpu_pt].x;
+            ay = instruction[cpu_pt].y;
+            setTime = instruction[cpu_pt].timer;
+            if (instruction[cpu_pt].led == 0)
+                led = false;
+            else
+                led = true;
+
+            cpu_timer.Enabled = true;
+            
+        }
+
+        //clear drone's memory
+        private void clear_instructions()
+        {
+            for (i = 0; i < 1000; i++)
+            {
+                instruction[0].x = 0;
+                instruction[0].y = 0;
+                instruction[0].timer = 0;
+                instruction[0].led = 0;
+            }
+        }
+
+        //----------------------------------------------------
+        //Graphical 
+        //----------------------------------------------------
+
+        private void graphical_update()
+        {
+            g.Clear(Color.Black);
+            for (i = 19; i > 0; i-- )
+                g.FillEllipse(trail, (int)map[i].x, (int)map[i].y, 10, 10);
+            
+            if (led)
+                g.FillEllipse(plane_led, (int)map[0].x, (int)map[0].y, 10, 10);  
+            else
+                 g.FillEllipse(plane, (int)map[0].x, (int)map[0].y, 10, 10);
+        }
+
+        //Physical system position update
+        private void position_update()
+        {
+            for (i = 19; i > 0; i--)
+            {
+                map[i] = map[i - 1];
+            }
+
+            px = r_x * ax + (1- r_x) * px;
+            map[0].x = map[0].x + px + random_x;
+
+            if (map[0].x < ulx)
+            {
+                map[0].x = ulx;
+                plane_crash();
+                return;
+            }
+            else if (map[0].x > brx )
+            {
+                map[0].x = brx;
+                plane_crash();
+                return;
+            }
+
+
+            py = r_y * ay + (1 - r_y) * py;
+            map[0].y = map[0].y + py + random_y;
+
+            if (map[0].y < uly)
+            {
+                map[0].y = uly;
+                plane_crash();
+                return;
+            }
+            else if (map[0].y > bry)
+            {
+                map[0].y = bry;
+                plane_crash();
+                return;
+            }
+         }
+
+        //reset the physical position
+        private void position_reset()
+        {
+            for (i = 0; i < 20; i++)
+            {
+                map[i].x = (double)center_x;
+                map[i].y = (double)center_y;
+            }
+        }
+
+        //drone crashed!
+        private void plane_crash()
+        {
+            timer2.Enabled = false;
+            cpu_timer.Enabled = false;
+            tbLog.Text += lblTime.Text + " Drone has crashed!" + Environment.NewLine;
+        }
+
+
+        //----------------------------------------------------------------------------------
+        //Timers
+        //----------------------------------------------------------------------------------
+
+        //timer to update the graphcial system
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphical_update();
+            if(start)
+            timer += 0.05;
+            lblTime.Text = timer.ToString("f1")+"s";
+        }
+
+
+        //timer to update the physical system
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (environment_random)
+            {
+                random_x = (r.NextDouble() - 0.5) * random_rate;
+                random_y = (r.NextDouble() - 0.5) * random_rate;
+            }
+
+            position_update();
+            
+        }
+
+        //drone internal cpu clock 
+        private void cpu_timer_Tick(object sender, EventArgs e)
+        {
+            cpu_instruction();
+        }
+
+        //initialize drone internal cpu
+        private void initalize_cpu()
+        {
+            cpu_counter = 0;
+            cpu_pt = -1;
+            setTime = 0;
+            cpu_timer.Enabled = true;
+        }
+
+
+        //---------------------------------------------------------------------------------
+        //driver layer masking
+        //---------------------------------------------------------------------------------
+        INSTRUCTION command_set(string input)
+        {
+            //instructions:
+            //MOVEX +/- 100
+            //MOVEY +/- 100
+            //WAIT
+
+            INSTRUCTION ins;
+            ins.x = 0;
+            ins.y = 0;
+            ins.timer = 0;
+            ins.led = 0;
+            try
+            {
+                string[] line = input.Split(' ', '\n', '\r');
+                if(debug)
+                {
+                  //  lblDEBUG.Text = line[0]+" "+line[1]+" "+line[2]+ " " +(Convert.ToInt32(line[1])+Convert.ToInt32(line[2]));
+                }
+                switch (line[0].ToUpper())
+                {
+                    case "MOVE": 
+                        {
+                             ins.x = Convert.ToInt32(line[1]);
+                             ins.y = Convert.ToInt32(line[2]);
+                            ins.timer = Convert.ToInt32(line[3]);
+                            ins.led = Convert.ToInt32(line[4]); 
+                            break;
+                        }
+                    case "WAIT": 
+                        { ins.x =  0; 
+                            ins.y = 0;
+                            ins.timer = Convert.ToInt32(line[1]);
+                            ins.led = Convert.ToInt32(line[2]); 
+                            break;
+                        }
+                         default: { ins.timer = -1; break; }
+                }
+                if (ins.x > 100)
+                    ins.x = 100;
+                else if (ins.x < -100)
+                    ins.x = -100;
+                
+                if (ins.y > 100)
+                    ins.y = 100;
+                else if (ins.y < -100) 
+                    ins.y = -100;
+
+                if (ins.timer > 30000)
+                    ins.timer = 30000;
+
+                if (ins.led !=0)
+                    ins.led = 1;
+
+            }
+            catch
+            {
+                ins.timer = -1;
+            }
+
+
+            if (debug)
+            {
+                lblDEBUG.Text = ins.x + " " + ins.y + " " + ins.timer;
+            }
+
+            return ins;
+        }
+
+
+
+        //---------------------------------------------------------------------------------
+    }
+}
